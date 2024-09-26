8a7bcfcae63bdf246a3234187720f49f0dcbe4aa
Josh Powell <jppowell0408@gmail.com>
2024-09-26 11:27:59 -0400

    Drop Jump Ratio completed

--- Module 3 - Time-Series Data/4 - Assignments/3.4.1 - Drop Jump/drop-jump-template.py
+++ Module 3 - Time-Series Data/4 - Assignments/3.4.1 - Drop Jump/drop-jump-template.py
@@ -16,11 +16,10 @@ def main(full_path_to_file):
     RSI: Calculated reactive strength index
     """
 
-
     # perform a basic check to see if the file exists. If not, exit the program
     if not path.exists(full_path_to_file):
         print("File does not exist", full_path_to_file)
-
+        return
     # load the data from the file
     data = np.loadtxt(full_path_to_file, delimiter=",")
 
@@ -33,10 +32,15 @@ def main(full_path_to_file):
     # Step 1: Establish a baseline by examining the force data the after for first ~20 points
 
     # set an amount of time to average and find the baseline
-    baseline_length = 0 ### your code here ###
+    baseline_length = 20  ### your code here ###
+    print("Baseline length:", baseline_length)
+
 
     # over the baseline, determine the average signal value
-    baseline = 0 ### your code here ###
+    baseline_values = force_plate[0:baseline_length]
+    print("Baseline values:", baseline_values)
+    baseline = np.mean(baseline_values, axis=0)
+    print("Average signal value:", baseline)
 
     # Step 2: After the baseline, find the first point that rises above that value
     # given some acceptable delta
@@ -48,7 +52,7 @@ def main(full_path_to_file):
     # index to time based upon the force plate sample rate
     first_landing_index = -1
 
-    # Approach 1: Make the array a list so we can iterate through it. There will be better and
+    # Approach 1: Make the array a list, so we can iterate through it. There will be better and
     # more Python friendly ways to do this but this is a general approach.
     force_plate_list = force_plate.tolist()
 
@@ -63,11 +67,15 @@ def main(full_path_to_file):
 
             ### your code here ###
 
+            first_landing_index = index
+            print("First landing point index:", first_landing_index)
+            print("First landing point:", force_plate_list[first_landing_index])
+
             # break out of the loop to end iterating
             break
 
     # Step 3: When force measurements return to the initial baseline the user has left the plate.
-    # Consider this the take off point.
+    # Consider this the take-off point.
 
     # when the signal falls below the baseline plus delta, that is the take off point
     delta = 5
@@ -81,12 +89,20 @@ def main(full_path_to_file):
     # then may accidentally find a point that is "too early" in the data
 
     # walk through the list but start a few moments after the at the landing index
-    # since we know the take off point will be afterwards.
+    # since we know the take-off point will be afterward.
     for index in range(first_landing_index + 10, len(force_plate_list)):
-
         ### your code here ###
-        delete_me = 0
+        # grab the current value in the list
+        value = force_plate_list[index]
 
+        if value < baseline + delta:
+            # mark this index as the landing point
+            take_off_index = index
+            print("Take off index:", take_off_index)
+            print("Take off point:", force_plate_list[take_off_index])
+
+            # break out of the loop to end iterating
+            break
 
     # Step 4: The plate should remain near baseline while the user is in the air (there is no load).
     # Once it rises above the baseline again, the user has landed. Consider this the second landing.
@@ -100,17 +116,34 @@ def main(full_path_to_file):
 
     # walk through the list but start a few moment after the takeoff point
     for index in range(take_off_index + 10, len(force_plate_list)):
-
         ### your code here ###
-        delete_me = 0
+        # grab the current value in the list
+        value = force_plate_list[index]
+
+        # if signal is rising
+        if value > baseline + delta:
+            # mark this index as the landing point
+
+            ### your code here ###
+
+            second_landing_index = index
+            print("Second landing point index:", second_landing_index)
+            print("Second landing point:", force_plate_list[second_landing_index])
+
+            # break out of the loop to end iterating
+            break
 
     # Step 5: calculate the time of contact on plate and time of flight in air
 
     # calculate tc and convert to seconds using the sampling rate
-    time_of_contact = 0 ### your code here ###
+    time_of_contact = 0  ### your code here ###
+    tc = (take_off_index-first_landing_index) / sampling_rate
+    print("Tc:", tc)
 
     # calculate tf and convert to seconds using the sampling rate
-    time_of_flight = 0 ### your code here ###
+    time_of_flight = 0  ### your code here ###
+    tf = (second_landing_index-take_off_index) / sampling_rate
+    print("Tf:", tf)
 
     # Step 6: Calculate the Reactive Strength Index
 
@@ -118,7 +151,9 @@ def main(full_path_to_file):
     g = constants.g
 
     # RSI = (g*tf^2) / (8*tc)
-    RSI = 0 ### your code here ###
+    RSI = 0  ### your code here ###
+    RSI = (g * tf ** 2) / (8 * tc)
+    print("RSI:", RSI)
 
     ### Do not modify below this line ###
 
@@ -129,15 +164,15 @@ def main(full_path_to_file):
 
 
 if __name__ == "__main__":
-    # need to import this here so it won't eventually affect the autograder
+    # need to import this here, so it won't eventually affect the autograder
     import matplotlib.pyplot as plt
 
     # change this file name to load other datasets
-    filename = "FP1.txt"
+    filename = r"\FP1.txt"
 
     # load force plate data (this path may change based upon where you place this file in your project)
-    path_to_data_folder = "../../../data/drop-jump/force-plate/"
-
+    path_to_data_folder = r"C:\Users\powelj\Documents\GitHub\engr340-fall2024\data\drop-jump\force-plate"
+    print("Data Folder Path:", path_to_data_folder)  # Print folder path for debug
     ### Do not modify below this line ###
 
     # put together directory and file to make a single relative path
