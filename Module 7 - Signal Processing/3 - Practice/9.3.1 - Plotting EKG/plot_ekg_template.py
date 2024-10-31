f1cf7d78d17c0ea1b7df638ce98b010f48bca6a2
Josh Powell <jppowell0408@gmail.com>
2024-10-31 11:27:30 -0400

    ekg plot and signal processing

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -7,12 +7,27 @@ path = '../../../data/ekg/mitdb_201.csv'
 
 # load data in matrix from CSV file; skip first two rows
 
-### Your code here ###
+ekg_data = np.array(np.loadtxt(path, delimiter=',', skiprows=2))
 
 # save each vector as own variable
 
-### Your code here ###
+elapsed_time = ekg_data[:,0]
+mlii = ekg_data[:,1]
+v1 = ekg_data[:,2]
+
 
 # use matplot lib to generate a single
 
-### Your code here ###
+plt.title('EKG Data - MLII')
+plt.xlabel('Time (s)')
+plt.ylabel('MLII (mV)')
+plt.plot(elapsed_time, mlii, label='MLII')
+plt.legend()
+plt.show()
+
+plt.title('EKG Data - V1')
+plt.xlabel('Time (s)')
+plt.ylabel('V1 (mV)')
+plt.plot(elapsed_time, v1, label='V1')
+plt.legend()
+plt.show()
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -18,8 +18,20 @@ signal_filepath = path_to_folder + database_name + ".csv"
 Step #1: load data in matrix from CSV file; skip first two rows. Call the data signal.
 """
 
-signal = 0
-## YOUR CODE HERE ##
+signal = np.array(np.loadtxt(signal_filepath, delimiter=',', skiprows=2))
+
+# Extract variables
+elapsed_time = signal[:,0]
+mlii = signal[:,1]
+v1 = signal[:,2]
+# Plot raw signal
+plt.title('Raw Signal')
+plt.xlabel('Time (s)')
+plt.ylabel('MLII and V1 (mV)')
+plt.plot(elapsed_time, mlii, label='MLII')
+plt.plot(elapsed_time, v1, label='V1')
+plt.legend()
+plt.show()
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
@@ -31,19 +43,48 @@ Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). The
 Step 3: Pass data through weighted differentiator
 """
 
-## YOUR CODE HERE ##
-
+mlii_diff = np.diff(mlii, prepend=mlii[0])
+v1_diff = np.diff(v1, prepend=v1[0])
+# Plot differentiated signal
+plt.title('Differentiated Signal')
+plt.xlabel('Time (s)')
+plt.ylabel('MLII and V1 (mV)')
+plt.plot(elapsed_time, mlii_diff, label='MLII')
+plt.plot(elapsed_time, v1_diff, label='V1')
+plt.legend()
+plt.show()
 
 """
 Step 4: Square the results of the previous step
 """
- ## YOUR CODE HERE ##
+
+mlii_squared = np.square(mlii)
+v1_squared = np.square(v1)
+# Plot squared signal
+plt.title('Squared Signal')
+plt.xlabel('Time (s)')
+plt.ylabel('MLII and V1 (mV)')
+plt.plot(elapsed_time, mlii_squared, label='MLII')
+plt.plot(elapsed_time, v1_squared, label='V1')
+plt.legend()
+plt.show()
 
 """
 Step 5: Pass a moving filter over your data
 """
 
-## YOUR CODE HERE
+window_size = 100
+moving_avg_mlii = np.convolve(mlii_squared, np.ones(window_size)/window_size, mode='same')
+moving_avg_v1 = np.convolve(v1_squared, np.ones(window_size)/window_size, mode='same')
+# Plot average signal
+plt.title('Average Signal')
+plt.xlabel('Time (s)')
+plt.ylabel('MLII and V1 (mV)')
+plt.plot(elapsed_time, moving_avg_mlii, label='MLII')
+plt.plot(elapsed_time, moving_avg_v1, label='V1')
+plt.legend()
+plt.show()
+
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
 plt.title('Process Signal for ' + database_name)
 plt.plot(signal)
