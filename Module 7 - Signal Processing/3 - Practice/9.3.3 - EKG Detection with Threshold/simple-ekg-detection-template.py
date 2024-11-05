acbfd23c5aa53325706934b0233a568542fb11eb
Josh Powell <jppowell0408@gmail.com>
2024-11-05 11:38:38 -0500

    Find Peaks and threshold ekg detection

--- Module 7 - Signal Processing/3 - Practice/9.3.3 - EKG Detection with Threshold/simple-ekg-detection-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.3 - EKG Detection with Threshold/simple-ekg-detection-template.py
@@ -23,7 +23,7 @@ Step 2: Determine how much data to use...
 """
 # If you wish to only run on ~10s of data uncomment the line below
 # if you wish to run on all data, comment out this line
-signal = signal[0:3300]
+signal = signal[0:9900]
 
 
 """
@@ -32,10 +32,10 @@ Adjust the values for threshold and timeout to change the detection method/appro
 """
 
 # set a detection threshold (YOUR VALUE BELOW)
-detection_threshold = -1
+detection_threshold = 2
 
 # set a heart beat time out (YOUR VALUE BELOW)
-detection_time_out = -1
+detection_time_out = 105
 
 # track the last time we found a beat
 last_detected_index = -1
@@ -52,9 +52,9 @@ Step 4: Manually iterate through the signal and apply the threshold with timeout
 
 # loop through signal finding beats
 for value in signal:
-    ## Use a conditional statement to see if the signal is above a threshold...
-
-    ## Once an index is found, place the index in the beats_detected list
+    if value >= detection_threshold and (current_index - last_detected_index) >= detection_time_out:
+        beats_detected.append(current_index)
+        last_detected_index = current_index
     current_index += 1
 
 print("Within the sample we found ", len(beats_detected), " heart beats with manual search!")
